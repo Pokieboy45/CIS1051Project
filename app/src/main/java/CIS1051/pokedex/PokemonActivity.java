a3f1cfb7dac6cba514c25bd8e8419f82c5aef6bc
Pokieboy45 <Blueber10@gmail.com>
2023-04-20 23:37:24 -0400

    Shiny sprite
    Added shiny sprite next to original sprite. Old phone no longer valid for desired operations.

--- app/src/main/java/CIS1051/pokedex/PokemonActivity.java
+++ app/src/main/java/CIS1051/pokedex/PokemonActivity.java
@@ -23,8 +23,8 @@ import org.json.JSONObject;
 public class PokemonActivity extends AppCompatActivity {
     private TextView nameTextView;
     private TextView numberTextView;
-    private TextView type1TextView;
-    private TextView type2TextView;
+    private TextView typesTextView;
+    //private TextView type2TextView;
     private String url;
     private RequestQueue requestQueue;
 
@@ -37,8 +37,8 @@ public class PokemonActivity extends AppCompatActivity {
         url = getIntent().getStringExtra("url");
         nameTextView = findViewById(R.id.pokemon_name);
         numberTextView = findViewById(R.id.pokemon_number);
-        type1TextView = findViewById(R.id.pokemon_type1);
-        type2TextView = findViewById(R.id.pokemon_type2);
+        typesTextView = findViewById(R.id.pokemon_types);
+        //type2TextView = findViewById(R.id.pokemon_type2);
 
         load();
 
@@ -47,27 +47,25 @@ public class PokemonActivity extends AppCompatActivity {
         JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject response) {
-                type1TextView.setText("");
-                type2TextView.setText("");
-
-
+                typesTextView.setText("");
 
                 try {
                     String NAME = response.getString("name");
                     nameTextView.setText(NAME.substring(0,1).toUpperCase()+NAME.substring(1));
                     numberTextView.setText(String.format("#%03d",response.getInt("id")));
                     JSONArray typeEntries = response.getJSONArray("types");
-
+                    String types = "";
                     for(int i = 0; i<typeEntries.length(); i++) {
                         JSONObject typeEntry = typeEntries.getJSONObject(i);
                         int slot = typeEntry.getInt("slot");
                         String type = typeEntry.getJSONObject("type").getString("name");
 
                         if (slot == 1 ) {
-                            type1TextView.setText(type.substring(0,1).toUpperCase()+type.substring(1));
+                            types = type.substring(0,1).toUpperCase()+type.substring(1);
                         } else if (slot == 2) {
-                            type2TextView.setText(type.substring(0,1).toUpperCase()+type.substring(1));
+                            types = types + "\t\t\t\t" + type.substring(0,1).toUpperCase()+type.substring(1);
                         }
+                        typesTextView.setText(types);
                     }
                 } catch (JSONException e) {
                     Log.e("CIS1051", "Pokemon Json error", e);
@@ -77,6 +75,10 @@ public class PokemonActivity extends AppCompatActivity {
                     Log.d("Test?", imagURL);
                     ImageView spriteView = findViewById(R.id.PokePic);;
                     Picasso.get().load(imagURL).into(spriteView);
+                    String shinyURL = response.getJSONObject("sprites").getJSONObject("other").getJSONObject("home").getString("front_shiny");
+                    Log.d("shiny url", shinyURL);
+                    ImageView shinyView = findViewById(R.id.PokePicShiny);;
+                    Picasso.get().load(shinyURL).into(shinyView);
                 } catch (JSONException e) {
                     Log.e("Image", "IMAGE URL ERROR", e);
                 }
